b316263c49c48cf4c0588b69ee2dc77deea1ed18
Mark Railton <mark@markrailton.com>
2021-02-08 17:30:44 +0000

    Sets up tailwind etc

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+
+@section('content')
+    <div>
+        <h1 class="text-gray-600 text-center font-light tracking-wider text-5xl mb-6">
+            Laravel Ireland
+        </h1>
+        <h2 class="text-gray-600 text-center font-light tracking-wider text-4xl mb-6">
+            coming soon!
+        </h2>
+    </div>
+@endsection
