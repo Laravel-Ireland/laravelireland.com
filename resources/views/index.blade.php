183a1c2bdaef730d76c863cebd67dba8353d52a8
Mark Railton <mark@markrailton.com>
2021-02-08 18:59:05 +0000

    Adds new font

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div>
-        <h1 class="text-gray-600 text-center font-light tracking-wider text-5xl mb-6">
+        <h1 class="text-gray-600 text-center font-light tracking-wider text-5xl mb-6 celtic-font">
             Laravel Ireland
         </h1>
-        <h2 class="text-gray-600 text-center font-light tracking-wider text-4xl mb-6">
+        <h2 class="text-gray-600 text-center font-light tracking-wider text-4xl mb-6 celtic-font">
             coming soon!
         </h2>
     </div>
