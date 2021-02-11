6bb29d2334e1d73e8f1f7d022656506e49e7b9aa
Mark Railton <mark@markrailton.com>
2021-02-11 11:25:23 +0000

    Making a start at layout

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    if (auth()->check()) {
-        return view('index');
-    }
-
-    return view('landing');
-});
+    return view('index');
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
