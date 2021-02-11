15cc46893e63243f96ceb9a325fbb6ecbd19108e
Mark Railton <mark@markrailton.com>
2021-02-11 10:40:11 +0000

    Adds laravel breeze

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,5 @@
-@extends('layout.app')
-
-@section('content')
+<x-guest-layout>
     <div>
-        <h1 class="text-gray-600 text-center font-light tracking-wider text-5xl mb-6 celtic-font">
-            Laravel Ireland
-        </h1>
-        <h2 class="text-gray-600 text-center font-light tracking-wider text-4xl mb-6 celtic-font">
-            coming soon!
-        </h2>
+        Index
     </div>
-@endsection
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    if (auth()->check()) {
+        return view('index');
+    }
+
+    return view('landing');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
