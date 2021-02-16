b80c720f73eee00832be6e3f399fd1a4f3c0ab92
Mark Railton <mark@markrailton.com>
2021-02-16 18:42:07 +0000

    Adds new landing page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,19 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200 celtic-font text-center">
-                    Welcome to Laravel Ireland
+    <div class="font-sans text-gray-900 antialiased">
+        <div class="flex flex-col">
+            <div class="min-h-screen flex items-center justify-center">
+                <div class="flex flex-col justify-around h-full">
+                    <div>
+                        <h1 class="text-gray-800 text-center font-light tracking-wider text-5xl mb-6 celtic-font">
+                            Laravel Ireland
+                        </h1>
+                        <h2 class="text-gray-800 text-center font-light tracking-wider text-3xl mb-6">
+                            We're currently working on a new website.
+                        </h2>
+                        <h3 class="text-gray-800 text-center font-light tracking-wider text-2xl mb-6">
+                            In the mean time, you can check us out on <a href="https://www.meetup.com/laravel-ireland/" class="hover:text-gray-500 hover:underline">Meetup</a> for upcoming events.
+                        </h3>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('home') }}">
                         <x-application-logo class="block h-10 w-auto fill-current text-gray-800"/>
                     </a>
                 </div>
@@ -14,9 +14,6 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
                         Home
-                    </x-nav-link>
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        Dashboard
                     </x-nav-link>
                 </div>
             </div>
