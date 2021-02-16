fd83e6d956768ea451f9b743e3303ee40b20768e
Mark Railton <mark@markrailton.com>
2021-02-16 18:45:42 +0000

    Adds GH action

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -12,6 +12,8 @@ class RegistrationTest extends TestCase
 
     public function test_registration_screen_can_be_rendered()
     {
+        $this->withoutExceptionHandling();
+
         $response = $this->get('/register');
 
         $response->assertStatus(200);
